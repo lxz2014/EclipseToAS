b99396d992e3c83d1f1a42afc1916cf129440033
lxz2014 <doujintianming@163.com>
2016-08-24 10:46:46 +0800

    添加忽略 lintOptions 错误配置

--- src/demo/lxz/eclipsetoas2/Main.java
+++ src/demo/lxz/eclipsetoas2/Main.java
@@ -2,6 +2,7 @@ package demo.lxz.eclipsetoas2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.BitSet;
 
 
 public class Main {
@@ -11,10 +12,7 @@ public class Main {
 	 */
 	public static void main(String[] args) throws FileNotFoundException {
 		EclipseToAs ea = new EclipseToAs();
-		ea.toAsProject(new File("E:/mywork/AndroidWork/xf_zhkt/AppStore/AppstoreAndroid"));
-		
-		//E:\mywork\AndroidWork\xf_realtime
-		//ea.toAsProject(new File("E:/mywork/AndroidWork/xf_realtime/"));
+		ea.toAsProject(new File("E:\\mywork\\AndroidWork\\xf_zhkt\\SmartClass"));
 	}
 
 	public static void log(String msg) {
